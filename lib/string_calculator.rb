662431a2c4d57df08e62e1f81e8e466c6e7bca94
Anisha Ramnani <anisha.ramnani@gmail.com>
2018-09-21 13:48:27 -0400

    take into account case where a number is passed

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,3 +1,7 @@
 def add str
-  0
+  if str == ""
+    0
+  elsif str.length == 1
+    str.to_i
+  end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,4 +3,8 @@ describe "#add" do
   it 'rerturns 0 for when given an empty string' do
     expect(add("")).to eq 0
   end
+
+  it 'returns a number when passed a single number' do
+    expect(add("6")).to eq(6)
+  end
 end
