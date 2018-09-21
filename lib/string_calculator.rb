8d439a6d2b80fdedce102be7d2531a07094cebb4
Anisha Ramnani <anisha.ramnani@gmail.com>
2018-09-21 15:16:46 -0400

    removes if statements since less code includes all cases

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,9 +1,3 @@
 def add str
-  if str.empty?
-    0
-  elsif str.length == 1
-    str.to_i
-  elsif str.length == 3
-    str.split(",").inject(0){ |sum,x| sum + x.to_i }
-  end
+  str.split(",").inject(0){ |sum,x| sum + x.to_i }
 end
